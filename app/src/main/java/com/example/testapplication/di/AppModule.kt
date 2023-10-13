baaf306f35cef220675598cfeafe07c69941087f
Nakibul Hassan <nakibul.hassan@bjitgroup.com>
2023-10-13 14:40:30 +0600

    code refactoring

--- app/src/main/java/com/example/testapplication/di/AppModule.kt
+++ app/src/main/java/com/example/testapplication/di/AppModule.kt
@@ -12,7 +12,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.lang.Boolean
 import javax.inject.Singleton
 import kotlin.String
 
@@ -21,33 +20,29 @@ import kotlin.String
 object AppModule {
 
     @Provides
-    fun provideBaseUrl() = Constants.BASE_URL2
+    fun provideBaseUrl() = Constants.BASE_URL
 
     @Singleton
     @Provides
-    fun provideOkHttpClient() = if (Boolean.parseBoolean("true")) {
+    fun provideOkHttpClient() = run {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .build()
-    } else {
-        OkHttpClient
-            .Builder()
-            .build()
     }
 
     @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL2: String): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String): Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(BASE_URL2)
+        .baseUrl(BASE_URL)
         .client(okHttpClient)
         .build()
 
     @Provides
     @Singleton
-    fun provideApiService(retrofit: Retrofit) = retrofit.create(ApiService::class.java)
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
 
     @Provides
     @Singleton
--- app/src/main/java/com/example/testapplication/utils/Constants.kt
+++ app/src/main/java/com/example/testapplication/utils/Constants.kt
@@ -1,11 +1,8 @@
 package com.example.testapplication.utils
 
 object Constants {
-
-    const val BASE_URL = "http://dummy.restapiexample.com/api/v1/"
-
     const val API_KEY = "64ca4704ac1f4fe5b58a0b5cb785ed3a"
-    const val BASE_URL2 = "https://newsapi.org"
+    const val BASE_URL = "https://newsapi.org"
     const val SEARCH_NEWS_TIME_DELAY = 500L
 
 }
